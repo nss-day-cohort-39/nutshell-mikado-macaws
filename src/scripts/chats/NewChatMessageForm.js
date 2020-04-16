cabfd1cc575590d331fe7768efe8ef357504adc5
Mark McCann <mjmccann12@me.com>
2020-04-16 11:18:06 -0500

    close to being done with NewChatMessageForm.js

--- src/scripts/chats/NewChatMessageForm.js
+++ src/scripts/chats/NewChatMessageForm.js
@@ -2,16 +2,26 @@
 
 
 const eventHub = document.querySelector(".container")
-
+// Creates the html string for a new chat
 export const chatMessageForm = () => {
     return `
         <section class="chat__form_container">
             <input class="chat__form_input">Enter your message here!!!</input>
-            <button class="chat__form_button">Submit</button>
+            <button id="chat__form_button">Submit</button>
         </section>
 `
 }
-
+// Dispatches the custom event of the submit button 
 eventHub.addEventListener("click", (e) => {
-
+    if (e.target.id === "chat__form_button") {
+        const chatMessage = document.querySelector(".chat__form_input").value
+        if (chatMessage) {
+            const clickEventOfSubmit = CustomEvent("eventOfSubmitButton", {
+                detail: {
+                    chatMessage: chatMessage,
+                }
+            })
+            eventHub.dispatchEvent(clickEventOfSubmit)
+        }
+    }
 })
