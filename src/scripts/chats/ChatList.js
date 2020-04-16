39110f58ab48c90020a583c3072c0059c1723889
Mark McCann <mjmccann12@me.com>
2020-04-16 14:12:10 -0500

    might be done with chat after I get out of bracket hell

--- src/scripts/chats/ChatList.js
+++ src/scripts/chats/ChatList.js
@@ -1,1 +1,34 @@
 // Author: Mark McCann
+
+// Module's purpose:
+// 1. Querry the DOM
+
+import { useChats } from "./ChatDataProvider.js"
+import { singleChatMessage } from "./Chat.js"
+import { chatMessageForm } from "./NewChatMessageForm.js"
+
+import { useUsers } from "../users/UsersDataProvider.js"
+
+const contentTarget = document.querySelector(".dashboard__chat")
+
+const render = (chatMessages) => {
+    const multipleUsers = useUsers()
+
+    contentTarget.innerHTML = `
+    <section class="preview__container">
+    ${chatMessages.map((chatMessage) => {
+        const singleUser = multipleUsers.find((singleUser) =>
+            singleUser.id === chatMessage.singleUserId)
+        return singleChatMessage(chatMessage, singleUser)
+    })
+            .join("")}
+            <section class="preview__container_form">${chatMessageForm()}</section>
+        }
+    </section>
+    `
+}
+
+export const chatMessageList = () => {
+    const chatMessages = useChats()
+    render(chatMessages)
+}
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,7 @@
 import { LoginForm } from "./users/UserLogin.js";
 import "./users/UserRegistration.js";
 import { getUsers } from "./users/UsersDataProvider.js";
+import { getChats } from "./chats/ChatDataProvider.js";
 
 getUsers().then(LoginForm);
+getChats()
