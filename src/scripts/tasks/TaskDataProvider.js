d0f7dd1bf60548cffb2c0c9d144b04be129611e1
BradCowart <Brad@TierStrategies.com>
2020-04-16 14:57:50 -0500

    Added Tasks

---
+++ src/scripts/tasks/TaskDataProvider.js
@@ -0,0 +1,44 @@
+/*
+Author: Brad Cowart
+Purpose: This module provides database access functionality. 
+*/
+
+let tasks = [];
+
+const eventHub = document.querySelector(".container");
+
+export const useTasks = () => tasks.slice();
+
+export const getTasks = () => {
+  return fetch("http://localhost:3000/tasks")
+    .then((response) => response.json())
+    .then((parsedTasks) => {
+      tasks = parsedTasks;
+    });
+};
+
+export const saveTasks = (tasks) => {
+  return fetch("http://localhost:3000/tasks", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(tasks),
+  })
+    .then(getTasks)
+    .then(dispatchStateChangeEvent);
+};
+
+export const deleteTasks = TaskId => {
+  return fetch(`http://localhost:8088/tasks/${Tasks.id}`, {
+      method: "DELETE"
+  })
+      .then(getTasks)
+      .then(dispatchStateChangeEvent);
+}
+
+const dispatchStateChangeEvent = () => {
+  const taskStateChangedEvent = new CustomEvent("taskStateChanged");
+
+  eventHub.dispatchEvent(taskStateChangedEvent);
+};
